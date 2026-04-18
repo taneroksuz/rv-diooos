898bdd361599f01c4b3069f82397281ef1b30a95
Taner Öksüz <taner.oeksuez@outlook.de>
2026-04-18 18:41:11 +0200

    blocking assignment removed.

--- verilog/rtl/rob.sv
+++ verilog/rtl/rob.sv
@@ -26,7 +26,6 @@ module rob (
   rob_entry_type view[0:ROB_DEPTH-1];
   rob_entry_type h0, h1;
   rob_entry_type alloc_entry0_w, alloc_entry1_w;
-  rob_entry_type tmp;
   logic h0_done, h1_done, h0_flush;
   logic commit0, commit1;
   logic alloc0_ok, alloc1_ok;
@@ -135,56 +134,48 @@ module rob (
       end
 
       if (rob_in.write_en0 && rin.valid_bits[rob_in.write_tag0]) begin
-        tmp            = array[rob_in.write_tag0];
-        tmp.done       = 1'b1;
-        tmp.result     = rob_in.write_entry0.result;
-        tmp.exception  = rob_in.write_entry0.exception;
-        tmp.ecause     = rob_in.write_entry0.ecause;
-        tmp.etval      = rob_in.write_entry0.etval;
-        tmp.npc        = rob_in.write_entry0.npc;
-        tmp.branch     = rob_in.write_entry0.branch;
-        tmp.jump       = rob_in.write_entry0.jump;
-        tmp.store_addr = rob_in.write_entry0.store_addr;
-        tmp.store_data = rob_in.write_entry0.store_data;
-        tmp.store_strb = rob_in.write_entry0.store_strb;
-        tmp.cwdata     = rob_in.write_entry0.cwdata;
-        array[rob_in.write_tag0] <= tmp;
+        array[rob_in.write_tag0].done       <= 1'b1;
+        array[rob_in.write_tag0].result     <= rob_in.write_entry0.result;
+        array[rob_in.write_tag0].exception  <= rob_in.write_entry0.exception;
+        array[rob_in.write_tag0].ecause     <= rob_in.write_entry0.ecause;
+        array[rob_in.write_tag0].etval      <= rob_in.write_entry0.etval;
+        array[rob_in.write_tag0].npc        <= rob_in.write_entry0.npc;
+        array[rob_in.write_tag0].branch     <= rob_in.write_entry0.branch;
+        array[rob_in.write_tag0].jump       <= rob_in.write_entry0.jump;
+        array[rob_in.write_tag0].store_addr <= rob_in.write_entry0.store_addr;
+        array[rob_in.write_tag0].store_data <= rob_in.write_entry0.store_data;
+        array[rob_in.write_tag0].store_strb <= rob_in.write_entry0.store_strb;
+        array[rob_in.write_tag0].cwdata     <= rob_in.write_entry0.cwdata;
       end
       if (rob_in.write_en1 && rin.valid_bits[rob_in.write_tag1]) begin
-        tmp            = array[rob_in.write_tag1];
-        tmp.done       = 1'b1;
-        tmp.result     = rob_in.write_entry1.result;
-        tmp.exception  = rob_in.write_entry1.exception;
-        tmp.ecause     = rob_in.write_entry1.ecause;
-        tmp.etval      = rob_in.write_entry1.etval;
-        tmp.npc        = rob_in.write_entry1.npc;
-        tmp.branch     = rob_in.write_entry1.branch;
-        tmp.jump       = rob_in.write_entry1.jump;
-        tmp.store_addr = rob_in.write_entry1.store_addr;
-        tmp.store_data = rob_in.write_entry1.store_data;
-        tmp.store_strb = rob_in.write_entry1.store_strb;
-        tmp.cwdata     = rob_in.write_entry1.cwdata;
-        array[rob_in.write_tag1] <= tmp;
+        array[rob_in.write_tag1].done       <= 1'b1;
+        array[rob_in.write_tag1].result     <= rob_in.write_entry1.result;
+        array[rob_in.write_tag1].exception  <= rob_in.write_entry1.exception;
+        array[rob_in.write_tag1].ecause     <= rob_in.write_entry1.ecause;
+        array[rob_in.write_tag1].etval      <= rob_in.write_entry1.etval;
+        array[rob_in.write_tag1].npc        <= rob_in.write_entry1.npc;
+        array[rob_in.write_tag1].branch     <= rob_in.write_entry1.branch;
+        array[rob_in.write_tag1].jump       <= rob_in.write_entry1.jump;
+        array[rob_in.write_tag1].store_addr <= rob_in.write_entry1.store_addr;
+        array[rob_in.write_tag1].store_data <= rob_in.write_entry1.store_data;
+        array[rob_in.write_tag1].store_strb <= rob_in.write_entry1.store_strb;
+        array[rob_in.write_tag1].cwdata     <= rob_in.write_entry1.cwdata;
       end
       if (rob_in.write_en2 && rin.valid_bits[rob_in.write_tag2]) begin
-        tmp           = array[rob_in.write_tag2];
-        tmp.done      = 1'b1;
-        tmp.result    = rob_in.write_entry2.result;
-        tmp.exception = rob_in.write_entry2.exception;
-        tmp.ecause    = rob_in.write_entry2.ecause;
-        tmp.etval     = rob_in.write_entry2.etval;
-        array[rob_in.write_tag2] <= tmp;
+        array[rob_in.write_tag2].done      <= 1'b1;
+        array[rob_in.write_tag2].result    <= rob_in.write_entry2.result;
+        array[rob_in.write_tag2].exception <= rob_in.write_entry2.exception;
+        array[rob_in.write_tag2].ecause    <= rob_in.write_entry2.ecause;
+        array[rob_in.write_tag2].etval     <= rob_in.write_entry2.etval;
       end
       if (rob_in.write_en3 && rin.valid_bits[rob_in.write_tag3]) begin
-        tmp            = array[rob_in.write_tag3];
-        tmp.done       = 1'b1;
-        tmp.store_addr = rob_in.write_entry3.store_addr;
-        tmp.store_data = rob_in.write_entry3.store_data;
-        tmp.store_strb = rob_in.write_entry3.store_strb;
-        tmp.exception  = rob_in.write_entry3.exception;
-        tmp.ecause     = rob_in.write_entry3.ecause;
-        tmp.etval      = rob_in.write_entry3.etval;
-        array[rob_in.write_tag3] <= tmp;
+        array[rob_in.write_tag3].done       <= 1'b1;
+        array[rob_in.write_tag3].store_addr <= rob_in.write_entry3.store_addr;
+        array[rob_in.write_tag3].store_data <= rob_in.write_entry3.store_data;
+        array[rob_in.write_tag3].store_strb <= rob_in.write_entry3.store_strb;
+        array[rob_in.write_tag3].exception  <= rob_in.write_entry3.exception;
+        array[rob_in.write_tag3].ecause     <= rob_in.write_entry3.ecause;
+        array[rob_in.write_tag3].etval      <= rob_in.write_entry3.etval;
       end
     end
   end
