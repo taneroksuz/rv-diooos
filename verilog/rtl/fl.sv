627007dd0387301c47213de0b17abcc4cf667be7
Taner Öksüz <taner.oeksuez@outlook.de>
2026-04-24 18:41:39 +0200

    units redesigned..

--- verilog/rtl/fl.sv
+++ verilog/rtl/fl.sv
@@ -10,117 +10,112 @@ module fl (
 );
   timeunit 1ns; timeprecision 1ps;
 
-  logic [PRF_ADDR_BITS-1:0] list[0:FLIST_DEPTH-1];
-  logic [FL_CNT_BITS-1:0] spec_head;
-  logic [FL_CNT_BITS-1:0] comm_head;
-  logic [FL_CNT_BITS-1:0] tail;
-  logic [FL_CNT_BITS-1:0] spec_count;
-  logic [FL_CNT_BITS-1:0] comm_count;
-  logic [FLIST_DEPTH-1:0] list_written;
+  typedef struct packed {
+    logic [FL_CNT_BITS-1:0] spec_head;
+    logic [FL_CNT_BITS-1:0] comm_head;
+    logic [FL_CNT_BITS-1:0] tail;
+    logic [FL_CNT_BITS-1:0] spec_count;
+    logic [FL_CNT_BITS-1:0] comm_count;
+    logic [FLIST_DEPTH-1:0] list_written;
+  } fl_reg_type;
+  localparam fl_reg_type init_fl_reg = '{
+      spec_head: '0,
+      comm_head: '0,
+      tail: FL_CNT_BITS'(FLIST_DEPTH),
+      spec_count: FL_CNT_BITS'(FLIST_DEPTH),
+      comm_count: FL_CNT_BITS'(FLIST_DEPTH),
+      list_written: '0
+  };
 
-  logic [FL_CNT_BITS-1:0] spec_head_n;
-  logic [FL_CNT_BITS-1:0] comm_head_n;
-  logic [FL_CNT_BITS-1:0] tail_n;
-  logic [FL_CNT_BITS-1:0] spec_count_n;
-  logic [FL_CNT_BITS-1:0] comm_count_n;
-  logic [FLIST_DEPTH-1:0] list_written_n;
+  logic [PRF_ADDR_BITS-1:0] list[0:FLIST_DEPTH-1];
+  fl_reg_type r, rin, v;
   logic do_free0, do_free1;
   logic [FL_IDX_BITS-1:0] free0_slot, free1_slot;
   logic [FL_IDX_BITS-1:0] alloc_slot0, alloc_slot1;
   logic [FL_CNT_BITS-1:0] spec_head_p1;
 
   always_comb begin
-    spec_head_n = spec_head;
-    comm_head_n = comm_head;
-    tail_n = tail;
-    spec_count_n = spec_count;
-    comm_count_n = comm_count;
-    list_written_n = list_written;
+    v = r;
     do_free0 = 1'b0;
     do_free1 = 1'b0;
     free0_slot = '0;
     free1_slot = '0;
 
-    alloc_slot0 = spec_head[FL_IDX_BITS-1:0];
-    spec_head_p1 = spec_head + FL_CNT_BITS'(1);
+    alloc_slot0 = r.spec_head[FL_IDX_BITS-1:0];
+    spec_head_p1 = r.spec_head + FL_CNT_BITS'(1);
     alloc_slot1 = spec_head_p1[FL_IDX_BITS-1:0];
 
     fl_out = '0;
-    fl_out.alloc_tag0 = list_written[alloc_slot0] ? list[alloc_slot0] : (PRF_ADDR_BITS'(ARCH_REGS) + PRF_ADDR_BITS'(alloc_slot0));
-    fl_out.alloc_tag1 = list_written[alloc_slot1] ? list[alloc_slot1] : (PRF_ADDR_BITS'(ARCH_REGS) + PRF_ADDR_BITS'(alloc_slot1));
-    fl_out.alloc_ok0 = (spec_count >= 1);
-    fl_out.alloc_ok1 = (spec_count >= 2);
-    fl_out.empty = (spec_count == '0);
-    fl_out.has_two = (spec_count >= 2);
+    fl_out.alloc_tag0 = r.list_written[alloc_slot0] ? list[alloc_slot0] : (PRF_ADDR_BITS'(ARCH_REGS) + PRF_ADDR_BITS'(alloc_slot0));
+    fl_out.alloc_tag1 = r.list_written[alloc_slot1] ? list[alloc_slot1] : (PRF_ADDR_BITS'(ARCH_REGS) + PRF_ADDR_BITS'(alloc_slot1));
+    fl_out.alloc_ok0 = (r.spec_count >= 1);
+    fl_out.alloc_ok1 = (r.spec_count >= 2);
+    fl_out.empty = (r.spec_count == '0);
+    fl_out.has_two = (r.spec_count >= 2);
 
     if (flush) begin
-      spec_head_n  = comm_head;
-      comm_head_n  = comm_head;
-      spec_count_n = comm_count;
+      v.spec_head  = r.comm_head;
+      v.comm_head  = r.comm_head;
+      v.spec_count = r.comm_count;
 
       if (fl_in.free_en0) begin
         do_free0 = 1'b1;
-        free0_slot = tail_n[FL_IDX_BITS-1:0];
-        tail_n = tail_n + 1'b1;
-        spec_head_n = spec_head_n + 1'b1;
-        comm_head_n = comm_head_n + 1'b1;
-        list_written_n[free0_slot] = 1'b1;
+        free0_slot = v.tail[FL_IDX_BITS-1:0];
+        v.tail = v.tail + 1'b1;
+        v.spec_head = v.spec_head + 1'b1;
+        v.comm_head = v.comm_head + 1'b1;
+        v.list_written[free0_slot] = 1'b1;
       end
 
       if (fl_in.free_en1) begin
         do_free1 = 1'b1;
-        free1_slot = tail_n[FL_IDX_BITS-1:0];
-        tail_n = tail_n + 1'b1;
-        spec_head_n = spec_head_n + 1'b1;
-        comm_head_n = comm_head_n + 1'b1;
-        list_written_n[free1_slot] = 1'b1;
+        free1_slot = v.tail[FL_IDX_BITS-1:0];
+        v.tail = v.tail + 1'b1;
+        v.spec_head = v.spec_head + 1'b1;
+        v.comm_head = v.comm_head + 1'b1;
+        v.list_written[free1_slot] = 1'b1;
       end
     end else begin
-      if (fl_in.free_en0 && (spec_count_n < FL_CNT_BITS'(FLIST_DEPTH))) begin
+      if (fl_in.free_en0 && (v.spec_count < FL_CNT_BITS'(FLIST_DEPTH))) begin
         do_free0 = 1'b1;
-        free0_slot = tail_n[FL_IDX_BITS-1:0];
-        tail_n = tail_n + 1'b1;
-        spec_count_n = spec_count_n + 1'b1;
-        comm_head_n = comm_head_n + 1'b1;
-        list_written_n[free0_slot] = 1'b1;
+        free0_slot = v.tail[FL_IDX_BITS-1:0];
+        v.tail = v.tail + 1'b1;
+        v.spec_count = v.spec_count + 1'b1;
+        v.comm_head = v.comm_head + 1'b1;
+        v.list_written[free0_slot] = 1'b1;
       end
 
-      if (fl_in.free_en1 && (spec_count_n < FL_CNT_BITS'(FLIST_DEPTH))) begin
+      if (fl_in.free_en1 && (v.spec_count < FL_CNT_BITS'(FLIST_DEPTH))) begin
         do_free1 = 1'b1;
-        free1_slot = tail_n[FL_IDX_BITS-1:0];
-        tail_n = tail_n + 1'b1;
-        spec_count_n = spec_count_n + 1'b1;
-        comm_head_n = comm_head_n + 1'b1;
-        list_written_n[free1_slot] = 1'b1;
+        free1_slot = v.tail[FL_IDX_BITS-1:0];
+        v.tail = v.tail + 1'b1;
+        v.spec_count = v.spec_count + 1'b1;
+        v.comm_head = v.comm_head + 1'b1;
+        v.list_written[free1_slot] = 1'b1;
       end
 
-      if (fl_in.alloc0 && (spec_count_n >= 1)) begin
-        spec_head_n  = spec_head_n + 1'b1;
-        spec_count_n = spec_count_n - 1'b1;
+      if (fl_in.alloc0 && (v.spec_count >= 1)) begin
+        v.spec_head  = v.spec_head + 1'b1;
+        v.spec_count = v.spec_count - 1'b1;
       end
-      if (fl_in.alloc1 && (spec_count_n >= 1)) begin
-        spec_head_n  = spec_head_n + 1'b1;
-        spec_count_n = spec_count_n - 1'b1;
+      if (fl_in.alloc1 && (v.spec_count >= 1)) begin
+        v.spec_head  = v.spec_head + 1'b1;
+        v.spec_count = v.spec_count - 1'b1;
       end
     end
+    rin = v;
   end
 
   always_ff @(posedge clock) begin
     if (reset == 0) begin
-      spec_head <= '0;
-      comm_head <= '0;
-      tail <= FL_CNT_BITS'(FLIST_DEPTH);
-      spec_count <= FL_CNT_BITS'(FLIST_DEPTH);
-      comm_count <= FL_CNT_BITS'(FLIST_DEPTH);
-      list_written <= '0;
+      r <= init_fl_reg;
     end else begin
-      spec_head <= spec_head_n;
-      comm_head <= comm_head_n;
-      tail <= tail_n;
-      spec_count <= spec_count_n;
-      comm_count <= comm_count_n;
-      list_written <= list_written_n;
+      r <= rin;
+    end
+  end
 
+  always_ff @(posedge clock) begin
+    if (reset != 0) begin
       if (do_free0) begin
         list[free0_slot] <= fl_in.free_tag0;
       end
--- verilog/rtl/prf.sv
+++ verilog/rtl/prf.sv
@@ -10,14 +10,18 @@ module prf (
 );
   timeunit 1ns; timeprecision 1ps;
 
+  typedef struct packed {logic [PRF_DEPTH-1:0] written_bits;} prf_reg_type;
+  localparam prf_reg_type init_prf_reg = '{written_bits: '0};
+
   logic [31:0] mem[0:PRF_DEPTH-1];
-  logic [PRF_DEPTH-1:0] written_bits;
+  prf_reg_type r, rin, v;
 
   logic wen0, wen1;
   logic [PRF_ADDR_BITS-1:0] waddr0, waddr1;
   logic [31:0] wdata0, wdata1;
 
   always_comb begin
+    v = r;
     wen0 = prf_in.wren0 && (prf_in.waddr0 != '0);
     waddr0 = prf_in.waddr0;
     wdata0 = prf_in.wdata0;
@@ -27,38 +31,51 @@ module prf (
 
     prf_out = init_prf_out;
 
-    prf_out.rdata0 = written_bits[prf_in.raddr0] ? mem[prf_in.raddr0] : 32'h0;
+    prf_out.rdata0 = r.written_bits[prf_in.raddr0] ? mem[prf_in.raddr0] : 32'h0;
     if (wen0 && waddr0 == prf_in.raddr0) prf_out.rdata0 = wdata0;
     else if (wen1 && waddr1 == prf_in.raddr0) prf_out.rdata0 = wdata1;
     prf_out.rvalid0 = 1'b1;
 
-    prf_out.rdata1  = written_bits[prf_in.raddr1] ? mem[prf_in.raddr1] : 32'h0;
+    prf_out.rdata1  = r.written_bits[prf_in.raddr1] ? mem[prf_in.raddr1] : 32'h0;
     if (wen0 && waddr0 == prf_in.raddr1) prf_out.rdata1 = wdata0;
     else if (wen1 && waddr1 == prf_in.raddr1) prf_out.rdata1 = wdata1;
     prf_out.rvalid1 = 1'b1;
 
-    prf_out.rdata2  = written_bits[prf_in.raddr2] ? mem[prf_in.raddr2] : 32'h0;
+    prf_out.rdata2  = r.written_bits[prf_in.raddr2] ? mem[prf_in.raddr2] : 32'h0;
     if (wen0 && waddr0 == prf_in.raddr2) prf_out.rdata2 = wdata0;
     else if (wen1 && waddr1 == prf_in.raddr2) prf_out.rdata2 = wdata1;
     prf_out.rvalid2 = 1'b1;
 
-    prf_out.rdata3  = written_bits[prf_in.raddr3] ? mem[prf_in.raddr3] : 32'h0;
+    prf_out.rdata3  = r.written_bits[prf_in.raddr3] ? mem[prf_in.raddr3] : 32'h0;
     if (wen0 && waddr0 == prf_in.raddr3) prf_out.rdata3 = wdata0;
     else if (wen1 && waddr1 == prf_in.raddr3) prf_out.rdata3 = wdata1;
     prf_out.rvalid3 = 1'b1;
+
+    if (wen0) begin
+      v.written_bits[waddr0] = 1'b1;
+    end
+    if (wen1) begin
+      v.written_bits[waddr1] = 1'b1;
+    end
+
+    rin = v;
   end
 
   always_ff @(posedge clock) begin
     if (reset == 0) begin
-      written_bits <= '0;
+      r <= init_prf_reg;
     end else begin
+      r <= rin;
+    end
+  end
+
+  always_ff @(posedge clock) begin
+    if (reset != 0) begin
       if (wen0) begin
         mem[waddr0] <= wdata0;
-        written_bits[waddr0] <= 1'b1;
       end
       if (wen1) begin
         mem[waddr1] <= wdata1;
-        written_bits[waddr1] <= 1'b1;
       end
     end
   end
